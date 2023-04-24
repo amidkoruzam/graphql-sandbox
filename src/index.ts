1a82649a55b11b71ac261b83caf555a5ff7be6ca
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-24 23:39:21 +0300

    change schema to books

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,30 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
   type Query {
-    hello: String
+    books: [Book]
   }
 `;
 
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
 const resolvers = {
   Query: {
-    hello: () => "world",
+    books: () => books,
   },
 };
 
